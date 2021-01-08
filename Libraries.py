5a4936f6e90e953e39a21fefa2c3e4e19b42ff98
AudreyDEGAUGUE <46415236+AudreyDEGAUGUE@users.noreply.github.com>
2021-01-08 14:42:35 +0100

    Add files via upload

---
+++ Libraries.py
@@ -0,0 +1,25 @@
+########## Module import ##########
+
+# Files
+from google.colab import files
+import json
+import pandas as pd
+
+# Maths
+import random
+
+# Extraction
+import re
+
+# Scraping
+import scrapy
+#from scrapy import Selector
+from requests import get
+
+# Parsing
+from urllib.parse import urlencode
+from urllib.parse import urlparse
+
+# Format
+import datetime
+import time
