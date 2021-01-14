69c878e80133885e487304ce6a6ccd6580de3351
AudreyDEGAUGUE <46415236+AudreyDEGAUGUE@users.noreply.github.com>
2021-01-14 10:00:29 +0100

    Add files via upload

---
+++ g2_Launch_All_Files_v2.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 12 10:42:23 2021
+
+@author: degau
+"""
+
+# Importing files
+from g2_Launch_Crawler_v2 import *
+
+
+# Launch crawling
+Launch_Crawler()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
