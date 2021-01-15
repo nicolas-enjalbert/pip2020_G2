35e019a9003bdd2211794b5d4b637c86a50df9ad
Maël <lesavoureym@gmail.com>
2021-01-15 13:29:44 +0100

    update.py quality

--- G2_serveur/Scrapping_nouvelles_sources/g2_update_json.py
+++ G2_serveur/Scrapping_nouvelles_sources/g2_update_json.py
@@ -9,10 +9,15 @@ Update tags repository for crawler
 
 import pandas as pd
 
-def add_line_json(link: str, infos: list):
+def add_line_json(path: str, info: list):
     """Documentation
+    
     Allows to add a line into the sources
+    
+    Parameters: 
+        path: String of the path to json file
+        info: List of information to be add on json file
     """
-    df: pd.DataFrame = pd.read_json(link)
-    df.loc[len(df)] = infos
-    df.to_json(link)
+    df: pd.DataFrame = pd.read_json(path)
+    df.loc[len(df)] = info
+    df.to_json(path)
